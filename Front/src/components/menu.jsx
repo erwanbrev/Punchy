5a1581544a7b25d11c7fccb2794d509943d09abb
Erwan Brévault <devbzh35@gmail.com>
2022-04-07 12:04:24 +0200

    redesign of the navbar

--- Front/src/components/menu.jsx
+++ Front/src/components/menu.jsx
@@ -13,10 +13,15 @@ export const Navbar = () => {
                 </div>
                 <div className="cont-Nav">
                     <Link to="/event" className="nameNav"><img
-                        src={require('../../public/assets/img/svg/evenement.svg')} alt="logo-event" /> <span id="event">Évènement</span> </Link>
+                        src={require('../../public/assets/img/svg/evenement.svg')} alt="logo-event" /> <span className="event">Évènement</span> </Link>
                     <Link to="/covoit" className="nameNav"><img src={require('../../public/assets/img/svg/covoit.svg')} alt="logo-covoit" />Covoiturage</Link>
                     <Link to="/geoloc" className="nameNav"><img src={require('../../public/assets/img/svg/itineraire.svg')} alt="logo-geoloc" />Location</Link>
-                    <Link to="/amis" className="nameNav"><img src={require('../../public/assets/img/svg/nb_personnes.svg')} alt="logo-geoloc" />Location</Link>
+                    <Link to="/amis" className="nameNav"><img src={require('../../public/assets/img/svg/nb_personnes.svg')} alt="logo-amis" />Amis</Link>
+                </div>
+                <div className="min-compte">
+                    <Link to="/compte">
+                        <img src={require('../../public/assets/img/svg/nb_personnes.svg')} alt="min-compte" /> <span className="event">Pseudo</span>
+                    </Link>
                 </div>
             </nav>
         </div>
