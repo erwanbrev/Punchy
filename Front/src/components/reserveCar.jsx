6bfe4fe1d6b3b5da95cbadc4a03aec9e95e612c2
nolannC <nolann.coll@gmail.com>
2022-04-13 23:35:58 +0200

    participate to a carpooling

--- Front/src/components/reserveCar.jsx
+++ Front/src/components/reserveCar.jsx
@@ -1,71 +1,100 @@
-import "../../public/assets/css/reserveCar.css"
-import user from "../../public/assets/img/user.png"
+import { useState, useEffect } from 'react';
+import '../../public/assets/css/reserveCar.css';
+import user from '../../public/assets/img/user.png';
 import img from '../../public/assets/img/imageCar.png';
 import line from '../../public/assets/img/svg/lineReserve.svg';
-import { IoStar, IoLogoEuro, IoTimeOutline, IoPerson } from "react-icons/io5";
+import { IoStar, IoLogoEuro, IoTimeOutline, IoPerson } from 'react-icons/io5';
+import { useNavigate } from 'react-router-dom';
 
+export const ReserveCar = ({ informations }) => {
+	const startDate = new Date(informations.startTime);
+	const day = startDate.getDate();
+	const month = startDate.getMonth() + 1;
+	const hours = startDate.getHours();
+	const minutes = startDate.getMinutes();
+	if (minutes < 10) {
+		minutes = '0' + minutes;
+	}
 
-export const ReserveCar = () => {
+	const startTime = day + '/' + month + ' ' + hours + ':' + minutes;
 
-        let profil = {
-        name: 'Stephen',
-        stars: 5,
-        depart: 'Dinard',
-        arrivee: 'Saint-Malo',
-        heure: '17h30',
-        prix: '3',
-        time: '15',
-        voiture: 'clio 3',
-        couleur: 'noir',
-        number: '2',
-    }
+	let note = informations.driver.notation.reduce((total, note) => total + note, 0) / informations.driver.notation.length;
 
-    return (
-        <div className='reserveCar'>
-            <img className="illustration" src={img} alt="illustration covoiturage" />
-            <div className="annonce">
-                <div className="row_">
-                    <img className="pProfil" src={user} alt="photo de profil" />
-                    <div className="monProfil">
-                        <p>{profil.name}</p>
-                        <div className="row_">
-                            <span>{profil.stars}</span>
-                            <IoStar color="FF7A00" size='35'/>
-                        </div>
-                    </div>
-                </div>
-                <div className="row_">
-                    <p>{profil.arrivee}</p>
-                    <img className="lineO" src={line} alt="ligne de trajet" />
-                    <p>{profil.depart}</p>
-                </div>
-                <div className="row_">
-                    <p>Départ:</p>
-                    <p className="depart">{profil.heure}</p>
-                </div>
-                <div className="row_">
-                    <p>Type de voiture:</p>
-                    <p>{profil.voiture}</p>
-                </div>
-                <div className="row_">
-                    <p>Couleur:</p>
-                    <p>{profil.couleur}</p>
-                </div>
-                <div className="row_">
-                    <p>Places disponibles: </p>
-                    <div className="row_">
-                        <IoPerson/>
-                        <p>{profil.number}</p>
-                    </div>
-                </div>
-                <div className="participe row_">
-                        <div className="prixCar v2">
-                        <span>{profil.prix}</span>
-                        <IoLogoEuro size="25"/>
-                    </div>
-                    <button className="btnAdd">Je participe</button>
-                </div>
-            </div>
-        </div>
-    )
-}
+	if (isNaN(note)) {
+		note = 'Ø';
+	}
+	const navigate = useNavigate();
+
+	const participate = () => {
+		if (!localStorage.getItem('access_token')) {
+			return navigate('/login', { replace: true });
+		}
+		fetch(`http://localhost:3000/carpooling/${informations.id}/participate`, {
+			headers: {
+				Authorization: 'Bearer ' + localStorage.getItem('access_token')
+			}
+		})
+			.then(response => {
+				return response.json();
+			})
+			.then(result => {
+				console.log(result);
+				alert('Votre participation a bien été enregistrer');
+			})
+			.catch(err => {
+				console.log(err.message);
+				alert('Le covoiturage est complet');
+			});
+	};
+
+	return (
+		<div className="reserveCar">
+			<img className="illustration" src={img} alt="illustration covoiturage" />
+			<div className="annonce">
+				<div className="row_">
+					<img className="pProfil" src={user} alt="photo de profil" />
+					<div className="monProfil">
+						<p>{informations.driver.fName}</p>
+						<div className="row_">
+							<span>{note}</span>
+							<IoStar color="FF7A00" size="35" />
+						</div>
+					</div>
+				</div>
+				<div className="row_">
+					<p>{informations.startLocalisation}</p>
+					<img className="lineO" src={line} alt="ligne de trajet" />
+					<p>{informations.endLocalisation}</p>
+				</div>
+				<div className="row_">
+					<p>Départ:</p>
+					<p className="depart">{startTime}</p>
+				</div>
+				<div className="row_">
+					<p>Type de voiture:</p>
+					<p>{informations.carType}</p>
+				</div>
+				<div className="row_">
+					<p>Couleur:</p>
+					<p>{informations.carColor}</p>
+				</div>
+				<div className="row_">
+					<p>Places disponibles: </p>
+					<div className="row_">
+						<IoPerson />
+						<p>{informations.peopleNumber}</p>
+					</div>
+				</div>
+				<div className="participe row_">
+					<div className="prixCar v2">
+						<span>{informations.price}</span>
+						<IoLogoEuro size="25" />
+					</div>
+					<button className="btnAdd" onClick={participate}>
+						Je participe
+					</button>
+				</div>
+			</div>
+		</div>
+	);
+};
