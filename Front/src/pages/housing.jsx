3013cc79eefa2e71a1753f047a741c5033523de4
Erwan Brévault <devbzh35@gmail.com>
2022-04-13 11:55:15 +0200

    add housing changes

--- Front/src/pages/housing.jsx
+++ Front/src/pages/housing.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import "../../public/assets/css/housing.css"
 import user from "../../public/assets/img/user.png"
 import picHouse from "../../public/assets/img/dinan-yannick-dufils-bN7Ae_grzZw-unsplash.jpeg"
-import { IoStar, IoLogoEuro, IoTimeOutline } from "react-icons/io5";
+import { IoStar, IoLogoEuro, IoLocationSharp, IoPieChart, IoPodium, IoStorefront } from "react-icons/io5";
 import { Searchbar } from "../components/searchbar";
 
 export const Housing = () => {
@@ -19,137 +19,41 @@ export const Housing = () => {
     }
 
     return (
-        <div className="cont-housing">
+        <div className="cont-house">
             <article className="art-Housing">
                 <img className="img-housing" src={picHouse} alt="Photo de Yannick Dufils sur Unsplash" />
+                {/* between est l'espace entre la colonne de gauche et de droite */}
                 <div className="between">
+                    {/* vertical permet de classer veticalement le contenu*/}
                     <div className="vertical">
                         <div className="contentHouse">
-                            <img className="userImage" src={user} alt="image de profil" />
                             <span>{contentHouse.name}</span>
-                            <div className="space">
+                            <div>
                                 <span>{contentHouse.stars}</span>
                                 <IoStar color="#FF7A00" />
                             </div>
                         </div>
-                        <div className="hDepart">
-                            <p>Localisation :</p>
+                        <div className="localization">
                             <span>{contentHouse.localization}</span>
+                            <IoLocationSharp />
+                        </div>
+                        <div className="surface">
+                            <span>{contentHouse.surface}</span>
+                            <IoPieChart />
+                        </div>
+                    </div>
+                    <div className="vertical">
+                        <div className="type">
+                            <p>{contentHouse.type}</p>
+                            <IoStorefront />
+                        </div>
+                        <div className="content">
+                            <p>{contentHouse.content}</p>
+                            <IoPodium />
                         </div>
                         <div className="price">
                             <span>{contentHouse.price}</span>
                             <IoLogoEuro />
-                        </div>
-                    </div>
-                    <div className="vertical">
-                        <div className="parcours">
-                            <span>{contentHouse.depart}</span>
-                        </div>
-                        <div className="time">
-                            <IoTimeOutline />
-                            <p>{contentHouse.time} min</p>
-                        </div>
-                    </div>
-                </div>
-            </article>
-            <article className="art-housing">
-                <img className="img-housing" src={picHouse} alt="affiche de voiture" />
-                <div className="between">
-                    <div className="vertical">
-                        <div className="contentHouse">
-                            <img className="userImage" src={user} alt="image de profil" />
-                            <span>{contentHouse.name}</span>
-                            <div className="space">
-                                <span>{contentHouse.stars}</span>
-                                <IoStar color="#FF7A00" />
-                            </div>
-                        </div>
-                        <div className="hDepart">
-                            <p>Départ:</p>
-                            <span>{contentHouse.heure}</span>
-                        </div>
-                        <div className="price">
-                            <span>{contentHouse.price}</span>
-                            <IoLogoEuro />
-                        </div>
-                    </div>
-                    <div className="vertical">
-                        <div className="parcour">
-                            <span>{contentHouse.depart}</span>
-                            {/* <img className="line" src={line} alt="" /> */}
-                            <span>{contentHouse.arrivee}</span>
-                        </div>
-                        <div className="time">
-                            <IoTimeOutline />
-                            <p>{contentHouse.time} min</p>
-                        </div>
-                    </div>
-                </div>
-            </article>
-            <article className="art-housing">
-                <img className="img-housing" src={picHouse} alt="affiche de voiture" />
-                <div className="between">
-                    <div className="vertical">
-                        <div className="contentHouse">
-                            <img className="userImage" src={user} alt="image de profil" />
-                            <span>{contentHouse.name}</span>
-                            <div className="space">
-                                <span>{contentHouse.stars}</span>
-                                <IoStar color="#FF7A00" />
-                            </div>
-                        </div>
-                        <div className="hDepart">
-                            <p>Départ:</p>
-                            <span>{contentHouse.heure}</span>
-                        </div>
-                        <div className="price">
-                            <span>{contentHouse.price}</span>
-                            <IoLogoEuro />
-                        </div>
-                    </div>
-                    <div className="vertical">
-                        <div className="parcour">
-                            <span>{contentHouse.depart}</span>
-                            <img className="line" src={line} alt="" />
-                            <span>{contentHouse.arrivee}</span>
-                        </div>
-                        <div className="time">
-                            <IoTimeOutline />
-                            <p>{contentHouse.time} min</p>
-                        </div>
-                    </div>
-                </div>
-            </article>
-            <article className="art-housing">
-                <img className="img-housing" src={picHouse} alt="affiche de voiture" />
-                <div className="between">
-                    <div className="vertical">
-                        <div className="contentHouse">
-                            <img className="userImage" src={user} alt="image de profil" />
-                            <span>{contentHouse.name}</span>
-                            <div className="space">
-                                <span>{contentHouse.stars}</span>
-                                <IoStar color="#FF7A00" />
-                            </div>
-                        </div>
-                        <div className="hDepart">
-                            <p>Départ:</p>
-                            <span>{contentHouse.heure}</span>
-                        </div>
-                        <div className="price">
-                            <span>{contentHouse.price}</span>
-                            <IoLogoEuro />
-                        </div>
-                    </div>
-                    <div className="vertical">
-                        <div className="parcour">
-                            <span>{contentHouse.depart}</span>
-                            <img className="line" src={line} alt="" />
-                            <span>{contentHouse.arrivee}</span>
-                        </div>
-                        <div className="time">
-                            <IoTimeOutline />
-                            <p>{contentHouse.time} min</p>
                         </div>
                     </div>
                 </div>
