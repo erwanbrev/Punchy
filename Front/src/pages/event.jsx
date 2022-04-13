870f29ba95056225c7a1139bd2b20154bb5acda5
Pierre Lem <pierre87.1997@gmail.com>
2022-04-13 11:17:33 +0200

    Update Event

--- Front/src/pages/event.jsx
+++ Front/src/pages/event.jsx
@@ -3,6 +3,7 @@ import { icons } from "react-icons";
 import "../../public/assets/css/evenement.css"
 import euro from "../../public/assets/img/svg/prix.svg"
 import image from "../../public/assets/img/bulles.jpg"
+import imagePerso from "../../public/assets/img/boite.jpg"
 import { CgEuro, CgSmartHomeBoiler } from 'react-icons/cg';
 
 
@@ -50,7 +51,45 @@ export const Event = () => {
             sombre.classList.toggle('clair');
         }
 
+        function popupinfoPerso(){
+            detaileventPerso.classList.toggle('hide');
+            sombrePerso.classList.toggle('clairPerso');
+        }
 
+        function popupinfoDeuxPerso(){
+            detaileventPerso.classList.toggle('hide');
+            sombrePerso.classList.toggle('clairPerso');
+        }
+
+        function popupinfoTroisPerso(){
+            detaileventPerso.classList.toggle('hide');
+            sombrePerso.classList.toggle('clairPerso');
+        }
+
+        function popupinfoQuatrePerso(){
+            detaileventPerso.PersoclassList.toggle('hide');
+            sombrePerso.PersoclassList.toggle('clairPerso');
+        }
+
+        function popupinfoCinqPerso(){
+            detaileventPerso.classList.toggle('hide');
+            sombrePerso.classList.toggle('clairPerso');
+        }
+
+        function popupinfoSixPerso(){
+            detaileventPerso.classList.toggle('hide');
+            sombrePerso.classList.toggle('clairPerso');
+        }
+
+        function popupinfoSeptPerso(){
+            detaileventPerso.classList.toggle('hide');
+            sombrePerso.classList.toggle('clairPerso');
+        }
+
+        function clairPerso(){
+            detaileventPerso.classList.toggle('hide');
+            sombrePerso.classList.toggle('clairPerso');
+        }
 
         Click.addEventListener('click', popupinfo)
         ClickDeux.addEventListener('click', popupinfoDeux)
@@ -60,6 +99,15 @@ export const Event = () => {
         ClickSix.addEventListener('click', popupinfoSix)
         ClickSept.addEventListener('click', popupinfoSept)
         sombre.addEventListener('click', clair)
+
+        ClickPerso.addEventListener('click', popupinfoPerso)
+        ClickPersoDeux.addEventListener('click', popupinfoDeuxPerso)
+        ClickPersoTrois.addEventListener('click', popupinfoTroisPerso)
+        ClickPersoQuatre.addEventListener('click', popupinfoQuatrePerso)
+        ClickPersoCinq.addEventListener('click', popupinfoCinqPerso)
+        ClickPersoSix.addEventListener('click', popupinfoSixPerso)
+        ClickPersoSept.addEventListener('click', popupinfoSeptPerso)
+        sombrePerso.addEventListener('click', clairPerso)
         }
     )
     
@@ -69,6 +117,14 @@ export const Event = () => {
         datedebut: '15 Janvier 2022',
         datefin: '21 Janvier 2022',
         lieu: '11 rue de la république, St Malo 35400'
+    }
+
+    let eventPerso = {
+        titre: 'Soirée Cinéma organisé par le BDE',
+        prix: '9,50',
+        datedebut: '15 Janvier 2022 20h30',
+        datefin: '15 Janvier 2022 22h30',
+        lieu: 'Cinedol, place du Foirail, 35120 Dol-de-Bretagne'
     }
 
     
@@ -127,13 +183,84 @@ export const Event = () => {
                 </div>
             </div>
             
-            <div className="hide" id="detailevent">
-                <span className="catNom">Nom :</span><span className="detailtitre">{event.titre}</span>
-                <span className="catLieu">Lieu :</span><span className="detaillieu">{event.lieu}</span>
-                <span className="catDebut">Début :</span><span className="detaildebut">{event.datedebut}</span>
-                <span className="catFin">Fin :</span><span className="detailfin">{event.datefin}</span>
-                <span className="catPrix">Prix :</span><span className="detailprix">{event.prix} €</span>
+            <div className="clairPerso" id="sombrePerso"></div>
+            <div className="eventPersocontainer">
+                <div className="EventPerso">
+                    <article className="evenementPerso">
+                        <span className="titre">{eventPerso.titre}</span>
+                        <img className="image" id="ClickPerso" src={imagePerso} alt="" srcset="" />
+                        <div className="euro"><CgEuro /></div><span className="prix">{eventPerso.prix}</span>
+                        <p className="participation PartiPerso">Je participe !</p>
+                    </article>
+
+                    <article className="evenementPerso">
+                        <span className="titre">{eventPerso.titre}</span>
+                        <img className="image" id="ClickPersoDeux" src={imagePerso} alt="" srcset="" />
+                        <div className="euro"><CgEuro /></div><span className="prix">{eventPerso.prix}</span>
+                        <p className="participation PartiPerso">Je participe !</p>
+                    </article>
+
+                    <article className="evenementPerso">
+                        <span className="titre">{eventPerso.titre}</span>
+                        <img className="image" id="ClickPersoTrois" src={imagePerso} alt="" srcset="" />
+                        <div className="euro"><CgEuro /></div><span className="prix">{eventPerso.prix}</span>
+                        <p className="participation PartiPerso">Je participe !</p>
+                    </article>
+                    <article className="evenementPerso">
+                        <span className="titre">{eventPerso.titre}</span>
+                        <img className="image" id="ClickPersoQuatre" src={imagePerso} alt="" srcset="" />
+                        <div className="euro"><CgEuro /></div><span className="prix">{eventPerso.prix}</span>
+                        <p className="participation PartiPerso">Je participe !</p>
+                    </article>
+
+                    <article className="evenementPerso">
+                        <span className="titre">{eventPerso.titre}</span>
+                        <img className="image" id="ClickPersoCinq" src={imagePerso} alt="" srcset="" />
+                        <div className="euro"><CgEuro /></div><span className="prix">{eventPerso.prix}</span>
+                        <p className="participation PartiPerso">Je participe !</p>
+                    </article>
+
+                    <article className="evenementPerso">
+                        <span className="titre">{eventPerso.titre}</span>
+                        <img className="image" id="ClickPersoSix" src={imagePerso} alt="" srcset="" />
+                        <div className="euro"><CgEuro /></div><span className="prix">{eventPerso.prix}</span>
+                        <p className="participation PartiPerso">Je participe !</p>
+                    </article>
+
+                    <article className="evenementPerso">
+                        <span className="titre">{eventPerso.titre}</span>
+                        <img className="image" id="ClickPersoSept" src={imagePerso} alt="" srcset="" />
+                        <span className="prix"> <CgEuro />{event.prix}</span>
+                        <p className="participation PartiPerso">Je participe !</p>
+                    </article>
+                </div>
             </div>
+
+
+            <div className="hide" id="detailevent">
+                <img className="imageDetail" src={image} alt="" srcset="" />
+                <span className="catNom grisTexte">Nom :</span><span className="detailtitre">{event.titre}</span>
+                <span className="catLieu grisTexte">Lieu :</span><span className="detaillieu">{event.lieu}</span>
+                <span className="catDebut grisTexte">Début :</span><span className="detaildebut">{event.datedebut}</span>
+                <span className="catFin grisTexte">Fin :</span><span className="detailfin">{event.datefin}</span>
+                <span className="catPrix grisTexte">Prix :</span><span className="detailprix">{event.prix} €</span>
+                <span id="catParticipant">Participants</span>
+                <button className="catParticipation">Je participe</button>
+
+
+            </div>
+
+            <div className="hide" id="detaileventPerso">
+                <img className="imageDetailPerso" src={imagePerso} alt="" srcset="" />
+                <span className="catPersoNom grisTexte">Nom :</span><span className="titrePerso detailPersotitre">{eventPerso.titre}</span>
+                <span className="catPersoLieu grisTexte">Lieu :</span><span className="titrePerso detailPersolieu">{eventPerso.lieu}</span>
+                <span className="catPersoDebut grisTexte">Début :</span><span className="titrePerso detailPersodebut">{eventPerso.datedebut}</span>
+                <span className="catPersoFin grisTexte">Fin :</span><span className="titrePerso detailPersofin">{eventPerso.datefin}</span>
+                <span className="catPersoPrix grisTexte">Prix :</span><span className="titrePerso detailPersoprix">{eventPerso.prix} €</span>
+                <span id="catPersoParticipant">Participants</span>
+                <button className="catPersoParticipation">Je participe</button>
+            </div>
+
         </div>            
     )
 }
