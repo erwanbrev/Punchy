f9bdbd6fb8fe287b57074756b57a365dfdffdfc5
Erwan Brévault <devbzh35@gmail.com>
2022-04-13 23:47:59 +0200

    push lastest version of housing.jsx/.css

--- Front/src/pages/housing.jsx
+++ Front/src/pages/housing.jsx
@@ -27,7 +27,7 @@ export const Housing = () => {
                     {/* vertical permet de classer veticalement le contenu*/}
                     {/* colonne de gauche */}
                     <div className="vertical-housing-left">
-                        <div className="cont-House">
+                        <div className="name-owner">
                             <span>{contentHouse.name}</span>
                         </div>
                         <div className="localization-housing">
@@ -35,7 +35,7 @@ export const Housing = () => {
                             <span>{contentHouse.localization}</span>
                         </div>
                         <div className="price-housing">
-                            <span>{contentHouse.price}</span>
+                            <p>{contentHouse.price}</p>
                             <IoLogoEuro className="price-logo" />
                         </div>
                     </div>
@@ -43,11 +43,11 @@ export const Housing = () => {
                     <div className="vertical-housing-right">
                         <div className="type-housing">
                             <IoStorefront color="var(--orange)" className="housing-logo" />
-                            <p>{contentHouse.type}</p>
+                            <span>{contentHouse.type}</span>
                         </div>
                         <div className="content-housing">
                             <IoPodium color="var(--orange)" className="content-logo" />
-                            <p>{contentHouse.content}</p>
+                            <span>{contentHouse.content}</span>
                         </div>
                         <div className="surface-housing">
                             <IoPieChart color="var(--orange)" className="surface-logo" />
@@ -64,7 +64,7 @@ export const Housing = () => {
                     {/* vertical permet de classer veticalement le contenu*/}
                     {/* colonne de gauche */}
                     <div className="vertical-housing-left">
-                        <div className="cont-House">
+                        <div className="name-owner">
                             <span>{contentHouse.name}</span>
                         </div>
                         <div className="localization-housing">
@@ -72,7 +72,7 @@ export const Housing = () => {
                             <span>{contentHouse.localization}</span>
                         </div>
                         <div className="price-housing">
-                            <span>{contentHouse.price}</span>
+                            <p>{contentHouse.price}</p>
                             <IoLogoEuro className="price-logo" />
                         </div>
                     </div>
@@ -80,11 +80,152 @@ export const Housing = () => {
                     <div className="vertical-housing-right">
                         <div className="type-housing">
                             <IoStorefront color="var(--orange)" className="housing-logo" />
-                            <p>{contentHouse.type}</p>
+                            <span>{contentHouse.type}</span>
                         </div>
                         <div className="content-housing">
                             <IoPodium color="var(--orange)" className="content-logo" />
-                            <p>{contentHouse.content}</p>
+                            <span>{contentHouse.content}</span>
+                        </div>
+                        <div className="surface-housing">
+                            <IoPieChart color="var(--orange)" className="surface-logo" />
+                            <span>{contentHouse.surface}</span>
+                        </div>
+                    </div>
+                </div>
+            </article>
+            <article className="art-Housing">
+                <img className="img-housing" src={picHouse} alt="Photo de Yannick Dufils sur Unsplash" />
+                {/* between est l'espace entre la colonne de gauche et de droite */}
+                <div className="between-housing">
+                    {/* vertical permet de classer veticalement le contenu*/}
+                    {/* colonne de gauche */}
+                    <div className="vertical-housing-left">
+                        <div className="name-owner">
+                            <span>{contentHouse.name}</span>
+                        </div>
+                        <div className="localization-housing">
+                            <IoLocationSharp color="var(--orange)" className="localiz-logo" />
+                            <span>{contentHouse.localization}</span>
+                        </div>
+                        <div className="price-housing">
+                            <p>{contentHouse.price}</p>
+                            <IoLogoEuro className="price-logo" />
+                        </div>
+                    </div>
+                    {/* colonne de droite */}
+                    <div className="vertical-housing-right">
+                        <div className="type-housing">
+                            <IoStorefront color="var(--orange)" className="housing-logo" />
+                            <span>{contentHouse.type}</span>
+                        </div>
+                        <div className="content-housing">
+                            <IoPodium color="var(--orange)" className="content-logo" />
+                            <span>{contentHouse.content}</span>
+                        </div>
+                        <div className="surface-housing">
+                            <IoPieChart color="var(--orange)" className="surface-logo" />
+                            <span>{contentHouse.surface}</span>
+                        </div>
+                    </div>
+                </div>
+            </article>    <article className="art-Housing">
+                <img className="img-housing" src={picHouse} alt="Photo de Yannick Dufils sur Unsplash" />
+                {/* between est l'espace entre la colonne de gauche et de droite */}
+                <div className="between-housing">
+                    {/* vertical permet de classer veticalement le contenu*/}
+                    {/* colonne de gauche */}
+                    <div className="vertical-housing-left">
+                        <div className="name-owner">
+                            <span>{contentHouse.name}</span>
+                        </div>
+                        <div className="localization-housing">
+                            <IoLocationSharp color="var(--orange)" className="localiz-logo" />
+                            <span>{contentHouse.localization}</span>
+                        </div>
+                        <div className="price-housing">
+                            <p>{contentHouse.price}</p>
+                            <IoLogoEuro className="price-logo" />
+                        </div>
+                    </div>
+                    {/* colonne de droite */}
+                    <div className="vertical-housing-right">
+                        <div className="type-housing">
+                            <IoStorefront color="var(--orange)" className="housing-logo" />
+                            <span>{contentHouse.type}</span>
+                        </div>
+                        <div className="content-housing">
+                            <IoPodium color="var(--orange)" className="content-logo" />
+                            <span>{contentHouse.content}</span>
+                        </div>
+                        <div className="surface-housing">
+                            <IoPieChart color="var(--orange)" className="surface-logo" />
+                            <span>{contentHouse.surface}</span>
+                        </div>
+                    </div>
+                </div>
+            </article>    <article className="art-Housing">
+                <img className="img-housing" src={picHouse} alt="Photo de Yannick Dufils sur Unsplash" />
+                {/* between est l'espace entre la colonne de gauche et de droite */}
+                <div className="between-housing">
+                    {/* vertical permet de classer veticalement le contenu*/}
+                    {/* colonne de gauche */}
+                    <div className="vertical-housing-left">
+                        <div className="name-owner">
+                            <span>{contentHouse.name}</span>
+                        </div>
+                        <div className="localization-housing">
+                            <IoLocationSharp color="var(--orange)" className="localiz-logo" />
+                            <span>{contentHouse.localization}</span>
+                        </div>
+                        <div className="price-housing">
+                            <p>{contentHouse.price}</p>
+                            <IoLogoEuro className="price-logo" />
+                        </div>
+                    </div>
+                    {/* colonne de droite */}
+                    <div className="vertical-housing-right">
+                        <div className="type-housing">
+                            <IoStorefront color="var(--orange)" className="housing-logo" />
+                            <span>{contentHouse.type}</span>
+                        </div>
+                        <div className="content-housing">
+                            <IoPodium color="var(--orange)" className="content-logo" />
+                            <span>{contentHouse.content}</span>
+                        </div>
+                        <div className="surface-housing">
+                            <IoPieChart color="var(--orange)" className="surface-logo" />
+                            <span>{contentHouse.surface}</span>
+                        </div>
+                    </div>
+                </div>
+            </article>    <article className="art-Housing">
+                <img className="img-housing" src={picHouse} alt="Photo de Yannick Dufils sur Unsplash" />
+                {/* between est l'espace entre la colonne de gauche et de droite */}
+                <div className="between-housing">
+                    {/* vertical permet de classer veticalement le contenu*/}
+                    {/* colonne de gauche */}
+                    <div className="vertical-housing-left">
+                        <div className="name-owner">
+                            <span>{contentHouse.name}</span>
+                        </div>
+                        <div className="localization-housing">
+                            <IoLocationSharp color="var(--orange)" className="localiz-logo" />
+                            <span>{contentHouse.localization}</span>
+                        </div>
+                        <div className="price-housing">
+                            <p>{contentHouse.price}</p>
+                            <IoLogoEuro className="price-logo" />
+                        </div>
+                    </div>
+                    {/* colonne de droite */}
+                    <div className="vertical-housing-right">
+                        <div className="type-housing">
+                            <IoStorefront color="var(--orange)" className="housing-logo" />
+                            <span>{contentHouse.type}</span>
+                        </div>
+                        <div className="content-housing">
+                            <IoPodium color="var(--orange)" className="content-logo" />
+                            <span>{contentHouse.content}</span>
                         </div>
                         <div className="surface-housing">
                             <IoPieChart color="var(--orange)" className="surface-logo" />
