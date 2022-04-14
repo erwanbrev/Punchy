56d5cb528775a62443d066ae20103074ed957147
AlexandreTouchet <alexandre.pro3525@gmail.com>
2022-04-14 15:27:06 +0200

    modif btn connexion dynamique

--- Front/src/Layout.jsx
+++ Front/src/Layout.jsx
@@ -1,12 +1,21 @@
-import React from "react";
-import { Outlet } from "react-router-dom";
+import React, { useEffect } from "react";
+import { Outlet, useNavigate } from "react-router-dom";
 import { Footer } from "./components/footer";
 import { Navbar } from "./components/menu";
 import { Outlet } from "react-router-dom";
 import { ButtonAdd } from "./components/buttonAdd";
+import { Authentification } from "./components/account/auth";
 
 
 export const Layout = () => {
+    const navigate = useNavigate()
+    useEffect(() => {
+        console.log('authLayout', Authentification.isAuthenticated());
+        if(!localStorage.getItem('token')) {
+            Authentification.authenticated = false
+        }
+        
+    }, [navigate])
 
     return (
         <div>
